87cc7817faec8b9711cbea15cf610171774284e8
RUS95 <38777519+RUS95@users.noreply.github.com>
2020-08-12 01:56:29 +0530

    add upload endpoint

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const redis = require("redis");
 const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
+const multer = require('multer');
+const inMemoryStorage = multer.memoryStorage();
+const uploadStrategy = multer({storage: inMemoryStorage}).single('image');
 
 const app = express();
 app.use(cors());
@@ -16,8 +19,8 @@ const client = redis.createClient({
 });
 
 // redis store configs
-const usersRedisKey = "store:users"; // cahce key for users
-const dataExpireTime = 3600; // 1 hour cache expire time
+const usersRedisKey = "store:users";    // cache key for users
+const dataExpireTime = 3600;            // 1 hour cache expire time
 
 // main endpoint
 app.get("/", (req, res) =>
@@ -50,6 +53,38 @@ app.get("/users", (req, res) => {
         });
     }
   });
+});
+
+app.post("/file/upload", uploadStrategy, (req, res) => {
+
+  try {
+    axios
+        .post("https://rus95-functionapp.azurewebsites.net/api/HttpTrigger1", {
+          filedata: req.file.buffer,
+          filename: req.file.originalname
+        })
+        .then((Res) => {
+          if (Res.status === 200) {
+            return res.status(200).json({
+              message: 'Image Uploaded!',
+              statusCode: 200
+            });
+          } else {
+            throw error;
+          }
+        })
+        .catch(e => {
+          return res.status(200).json({
+            message: 'Image Upload failed!',
+            statusCode: 400
+          });
+          console.error(e);
+        });
+
+  } catch (e) {
+    console.error(e);
+  }
+
 });
 
 // start express server
